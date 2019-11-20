33c5f5dad3729cf98496cfa75411fe1569366698
Jan-Samuel Vogelainen <samuel@Jan-Samuels-Air.lan>
2019-11-20 13:15:14 +0200

    continue operaatori kasutamise näide -paaris arvud

--- app.js
+++ app.js
@@ -4,16 +4,11 @@
  let lause;
  for(let arv = 1; arv <= 10; arv++){
    lause = 'arv =  ';
-   if(arv < 10) {
-     console.log('arv =  ' + arv);
-     lause = lause + ' ';
-   }
-   lause = lause + arv;
-   if(arv % 2 == 0){
+ @@ -14,6 +13,7 @@ for(let arv = 1; arv <= 10; arv++){
      lause = lause + ' - paaris';
    } else {
-     console.log('arv = ' + arv);
      lause = lause + ' - paaritu';
+     continue;
    }
    console.log(lause);
  } 
