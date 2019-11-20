3c87392306f1bc3df5e6c46d4394ec5eb2f81948
Jan-Samuel Vogelainen <samuel@Jan-Samuels-Air.lan>
2019-11-20 13:27:00 +0200

    Paarsus ja väljastus eraldi funktisoonidega

--- app.js
+++ app.js
@@ -1,43 +1,28 @@
-// Funktsioonid
+// Funktsioon, mis arvutab 
+ // riistküliku pindala
+ // ja väljastab ekraani
 
- // arvu paarsuse kontroll
- // funktsiooni defineerimine
- /* 
- function funktsiooniNimi([param1, param2, .., paramN]){
-   funktsiooni sisu
- }
- */
-function paarsuseKontroll(arv){
-    // tulemuslause defineerimine
-    let lause;
-    // arvutame jääk 2-ga jagamisel
-    let jaak = arv % 2;
-    // kui jaak on võrdne nulliga
-    if( jaak === 0 ){
-      // tegu on paaris avuga
-      lause =  arv + ' on paaris';
-    } else {
-      // tegu on paaritu arvuga
-      lause =  arv + ' on paaritu';
-    }
-    // tagastame antud väärtus
-    return lause;
-  // Funktsioon, mis arvutab 
-  // riistküliku pindala
-  // ja väljastab ekraani
+ function riistkylikuPindala(pikkus, laius){
+    // arvutame pindala
+    let pindala = pikkus * laius;
+    // tagastame pindala põhiprogrammi
+    return pindala;
+  }
  
-  function riistkylikuPindala(pikkus, laius){
+  // Funktsioon, mis väljastab tulemus ekraani
+  function valjasta(pikkus, laius){
     // väljastame väärtused
     console.log('Pikkus = ' + pikkus);
     console.log('Laius = ' + laius);
     // arvutame pindala
     let pindala = pikkus * laius;
     // väljastame pindala
+    // salvestame arvutatud pindala
+    let pindala = riistkylikuPindala(pikkus, laius);
+    // väljastame tulemus
     console.log('Pindala = ' + pindala);
   }
-  // funktsiooni kasutamine
-  let paarsus;
-  paarsus = paarsuseKontroll(4);
-  console.log(paarsus);
  
   riistkylikuPindala(15, 5);
+  valjasta(15, 5);
+  valjasta(1, 5); 
