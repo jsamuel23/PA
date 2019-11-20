9aa1bbcd20231e89dc64c6a4a313c1445dcee5e2
Jan-Samuel Vogelainen <samuel@Jan-Samuels-Air.lan>
2019-11-20 13:12:53 +0200

    for - numbrid 1st 10ni

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-// Logid konsoolile
- // Muutujad
+// Muutujad
 
- // logid
- console.log('123');
  // var
  // muutujate defineerimine
  var nimi = 'Anna'; // defineerimine
@@ -10,45 +7,29 @@
  nimi = 'Katrin'; // väärtuse omistamine
  console.log(nimi);
 
- // tabel
- console.table({a : 1, b : 2});
  // muutujate initsialiseerimine
  var tervitus;
  console.log(tervitus);
  tervitus = 'Tere tulemast!';
  console.log(tervitus);
 
- // veateade
- console.error('See on mingi viga');
  // liitnimede kasutamine
  var eesNimi = 'Anna'; // Camel Stiil
  var ees_nimi = 'Anna'; // PHP Stiil
  var EesNimi = 'Anna'; // Pascal Stiil
  var eesnimi = 'Anna';
 
- // märkused
- console.warn('See on märkus');
  // let - muutuja
  let pereNimi = 'Karutina';
  console.log(pereNimi);
  pereNimi = '?????';
  console.log(pereNimi);
 
- // ajakulu kontroll
- console.time('Test');
-   console.log('mingi tegevus');
-   console.log('mingi tegevus');
-   console.log('mingi tegevus');
-   console.log('mingi tegevus');
-   console.log('mingi tegevus');
- console.timeEnd('Test');
  // const - konstant
  const minuEesNimi = 'Anna';
  console.log(minuEesNimi);
  // minuEesNimi = 'Katrin';
 
- // kustutamine
- console.clear(); 
  // const kasutamise näide 1
  const inimene = {nimi : 'Katrin', vanus : 20};
  // inimene = {nimi: 'Katrin', rahvus: 'eestlane'};
@@ -62,3 +43,15 @@
  console.log(arvud);
  arvud.push(6);
  console.log(arvud); 
+ // Tsüklid
+
+ // for tsükkel
+ // üldtsükkel
+ // let arv;
+ for(let arv = 1; arv <= 10; arv++){
+   if(arv < 10) {
+     console.log('arv =  ' + arv);
+   } else {
+     console.log('arv = ' + arv);
+   }
+ } 
