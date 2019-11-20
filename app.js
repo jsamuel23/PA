dd71450565b1f307a9ebf3c46011f122566dca37
Jan-Samuel Vogelainen <samuel@Jan-Samuels-Air.lan>
2019-11-20 13:26:21 +0200

    Riistküliku pindala arvutamine koos väljastamisega

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-@@ -8,17 +8,22 @@ function funktsiooniNimi([param1, param2, .., paramN]){
-}
-*/
+// Funktsioonid
+
+ // arvu paarsuse kontroll
+ // funktsiooni defineerimine
+ /* 
+ function funktsiooniNimi([param1, param2, .., paramN]){
+   funktsiooni sisu
+ }
+ */
 function paarsuseKontroll(arv){
-  // tulemuslause defineerimine
-  let lause;
-  // arvutame jääk 2-ga jagamisel
-  let jaak = arv % 2;
-  // kui jaak on võrdne nulliga
-  if( jaak === 0 ){
-    // tegu on paaris avuga
-    console.log(arv + ' on paaris');
-    lause =  arv + ' on paaris';
-  } else {
-    // tegu on paaritu arvuga
-    console.log(arv + ' on paaritu');
-    lause =  arv + ' on paaritu';
+    // tulemuslause defineerimine
+    let lause;
+    // arvutame jääk 2-ga jagamisel
+    let jaak = arv % 2;
+    // kui jaak on võrdne nulliga
+    if( jaak === 0 ){
+      // tegu on paaris avuga
+      lause =  arv + ' on paaris';
+    } else {
+      // tegu on paaritu arvuga
+      lause =  arv + ' on paaritu';
+    }
+    // tagastame antud väärtus
+    return lause;
+  // Funktsioon, mis arvutab 
+  // riistküliku pindala
+  // ja väljastab ekraani
+ 
+  function riistkylikuPindala(pikkus, laius){
+    // väljastame väärtused
+    console.log('Pikkus = ' + pikkus);
+    console.log('Laius = ' + laius);
+    // arvutame pindala
+    let pindala = pikkus * laius;
+    // väljastame pindala
+    console.log('Pindala = ' + pindala);
   }
-  // tagastame antud väärtus
-  return lause;
-}
-// funktsiooni kasutamine
-paarsuseKontroll(4);
-paarsuseKontroll(9);
-let paarsus;
-paarsus = paarsuseKontroll(4);
-console.log(paarsus);
+  // funktsiooni kasutamine
+  let paarsus;
+  paarsus = paarsuseKontroll(4);
+  console.log(paarsus);
+ 
+  riistkylikuPindala(15, 5);
