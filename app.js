24da8f3a161f93aa805aef38d3ffdf965a8d3dd5
Jan-Samuel Vogelainen <samuel@Jan-Samuels-Air.lan>
2019-11-20 13:06:09 +0200

    Konsooli kasutamisvõimalused info väljastamiseks

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
-console.log("JS test"); 
 // Andmetüübid
+ // Logid konsoolile
 
-// täisarvud
-console.log(10);
-console.log(-4);
-console.log(0);
+ // täisarvud
+ console.log(10);
+ console.log(-4);
+ console.log(0);
+ // logid
+ console.log('123');
 
-// reaalarvud - ujuvkomaarvud
-console.log(2.5);
-console.log(-7.23);
-console.log(-2.0);
+ // reaalarvud - ujuvkomaarvud
+ console.log(2.5);
+ console.log(-7.23);
+ console.log(-2.0);
+ // tabel
+ console.table({a : 1, b : 2});
 
-// sõned - tekst
-console.log('Hello World!');
+ // sõned - tekst
+ console.log('Hello World!');
+ // veateade
+ console.error('See on mingi viga');
 
-// tõeväärtused
-console.log(true); // tõene
-console.log(false); // väär
+ // tõeväärtused
+ console.log(true); // tõene
+ console.log(false); // väär
+ // märkused
+ console.warn('See on märkus');
 
-// nimekiri 
-console.log([1, 2, 3, 4]);
+ // nimekiri 
+ console.log([1, 2, 3, 4]);
+ // ajakulu kontroll
+ console.time('Test');
+   console.log('mingi tegevus');
+   console.log('mingi tegevus');
+   console.log('mingi tegevus');
+   console.log('mingi tegevus');
+   console.log('mingi tegevus');
+ console.timeEnd('Test');
 
-// objektid
-console.log({a : 1, b : 2});
-console.table({a : 1, b : 2}); 
+ // objektid
+ console.log({a : 1, b : 2});
+ console.table({a : 1, b : 2}); 
+ // kustutamine
+ console.clear(); 
+
