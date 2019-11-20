1ec87ecdcf98109b9b80477a4682ff2148cf44bd
Jan-Samuel Vogelainen <samuel@Jan-Samuels-Air.lan>
2019-11-20 13:16:08 +0200

    break kasutamise näide - ainult 5 esitmest arvu

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-// for tsükkel
- // üldtsükkel
- // let arv;
- let lause;
- for(let arv = 1; arv <= 10; arv++){
-   lause = 'arv =  ';
- @@ -14,6 +13,7 @@ for(let arv = 1; arv <= 10; arv++){
-     lause = lause + ' - paaris';
-   } else {
-     lause = lause + ' - paaritu';
-     continue;
-   }
-   console.log(lause);
- } 
+lause = lause + ' - paaris';
+} else {
+  lause = lause + ' - paaritu';
+  continue;
+}
+console.log(lause);
+if(arv >= 5) {
+  break;
+}
+} 
