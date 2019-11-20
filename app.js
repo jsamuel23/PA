1701d0ec9741577203b2281ccd98ed8a9ce2c205
Jan-Samuel Vogelainen <samuel@Jan-Samuels-Air.lan>
2019-11-20 13:24:29 +0200

    Paarsus kontroll funktisooni abil

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-// Tsüklid
- // Funktsioonid
+// Funktsioonid
 
- // while
- // ellkontrolliga tsükkel
- // defineerime number
- let arv = 4; 
- // defineerime jagaja algväärtus
- let jagaja = 2;
- // suurendame jagaja väärtus nii kaua, kui
- // jagamise jääk on 0
- while(arv % jagaja != 0){
-   jagaja++;
- }
- // kontrollime, kas jagasime sama arvuga
- if(arv == jagaja) {
-   console.log(arv + ' on algarv');
  // arvu paarsuse kontroll
  // defineerime suvaline täisarv
  let arv = 5;
@@ -25,7 +10,26 @@
    // tegu on paaris avuga
    console.log(arv + ' on paaris');
  } else {
-   console.log(arv + ' ei ole algarv');
    // tegu on paaritu arvuga
    console.log(arv + ' on paaritu');
+ // funktsiooni defineerimine
+ /* 
+ function funktsiooniNimi([param1, param2, .., paramN]){
+   funktsiooni sisu
  }
+ */
+ function paarsuseKontroll(arv){
+   // arvutame jääk 2-ga jagamisel
+   let jaak = arv % 2;
+   // kui jaak on võrdne nulliga
+   if( jaak === 0 ){
+     // tegu on paaris avuga
+     console.log(arv + ' on paaris');
+   } else {
+     // tegu on paaritu arvuga
+     console.log(arv + ' on paaritu');
+   }
+ }
+ // funktsiooni kasutamine
+ paarsuseKontroll(4);
+ paarsuseKontroll(9);
